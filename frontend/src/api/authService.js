f1d6e5ee0a30a5de3e8470b94155999acc1750a3
John-Ravi934 <johnravi934@gmail.com>
2026-01-22 14:51:42 +0530

    Successfully added axios.js, authService.js

---
+++ frontend/src/api/authService.js
@@ -0,0 +1,15 @@
+import api from "./axios";
+
+export const login = async (email, password) => {
+  const res = await api.post("/auth/login", {
+    email,
+    password,
+  });
+
+  localStorage.setItem("token", res.data.token);
+  return res.data;
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+};
