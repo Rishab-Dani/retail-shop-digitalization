07f89f99343644bb1b036110a9c4853fb83dbeb0
John-Ravi934 <johnravi934@gmail.com>
2026-01-31 23:45:59 +0530

    Connecting APi to Customers Profile

--- frontend/src/api/customerApi.js
+++ frontend/src/api/customerApi.js
@@ -1,26 +1,26 @@
 import api from "./axios";
 
-// Backend-confirmed endpoints
+/* PROFILE */
 export const getMyProfile = () =>
   api.get("/api/customer/profile");
-
-export const getMyOrders = (params) =>
-  api.get("/api/customer/orders", { params });
-
-export const getMyAddresses = () =>
-  api.get("/api/customer/addresses");
 
 export const updateProfile = (data) =>
   api.put("/api/customer/profile", data);
 
+/* ORDERS */
+export const getMyOrders = () =>
+  api.get("/api/customer/orders");
 
-/* ADDRESS APIs */
+/* ADDRESSES */
+export const getMyAddresses = () =>
+  api.get("/api/customer/addresses");
+
 export const createAddress = (data) =>
-  api.post("/customers/me/addresses", data);
+  api.post("/api/customer/addresses", data);
 
 export const updateAddress = (id, data) =>
-  api.put(`/customers/me/addresses/${id}`, data);
+  api.put(`/api/customer/addresses/${id}`, data);
 
 export const removeAddress = (id) =>
-  api.delete(`/customers/me/addresses/${id}`);
+  api.delete(`/api/customer/addresses/${id}`);
 
